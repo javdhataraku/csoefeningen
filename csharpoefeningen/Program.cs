9f09a3590af9137da2f1290a038172106ef50fc4
javdhataraku <javdhataraku>
2022-02-10 09:52:20 +0100

    oefeningen

---
+++ csharpoefeningen/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CsharpOefeningen
+{
+    class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            //zet de oefening aan die je wil testen en RUN het programma (play knop) om te testen
+
+            Variabelen  variabelen = new Variabelen();
+
+            //variabelen.Oefening0();
+            //variabelen.Oefening1();
+            //variabelen.Oefening2();
+
+
+
+            //Classes classes = new Classes();
+            //classes.RunOefeningen();
+
+
+            Console.ReadLine();//laat het programma niet beeindigen zodat je het kan zien
+        }
+    }
+}
